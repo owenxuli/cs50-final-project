096524cbc061d11cd53a8f67b8f89899b09d800c
owenxuli <xuliowen605@gmail.com>
2023-12-01 14:57:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
+import chess
 
 from helpers import apology, login_required
 
