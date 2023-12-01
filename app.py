ff8eac2a91554ef357020759b917ff94420fb3e7
owenxuli <xuliowen605@gmail.com>
2023-11-30 20:28:33 -0500

    now it works

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ from helpers import apology, login_required
 # Configure application
 app = Flask(__name__)
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///users.db")
 
@@ -28,9 +33,7 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-    return render_template(
-        "homepage.html"
-    )
+    return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
