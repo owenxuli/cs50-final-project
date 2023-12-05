eb4edffb1e1d54faa6c7cff73c038d58e6034a9a
DanielZhangg <143767413+DanielZhangg@users.noreply.github.com>
2023-12-04 22:41:56 -0500

    d

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///users.db")
+db = SQL("sqlite:///openings.db")
 
 
 @app.after_request
