4487e8564d7985aab8a5be538090faf56ee5fb1b
owenxuli <xuliowen605@gmail.com>
2023-12-09 15:57:09 -0500

    s

--- static/js/board3.js
+++ static/js/board3.js
@@ -3,13 +3,25 @@ $(document).ready(function () {
         position: 'start',
     
     });
-$('#move1Btn').on('click', function () {
-board3.move('e2-e4')
+$('#3move1').on('click', function () {
+    board3.move('e2-e4')
 })
 
-$('#move2Btn').on('click', function () {
-board3.move('d2-d4', 'g8-f6')
+$('#3move2').on('click', function () {
+    board3.move('e7-e5')
 })
 
-$('#startPositionBtn').on('click', board3.start)
+$('#3move3').on('click', function () {
+    board3.move('g1-f3')
+})
+
+$('#3move4').on('click', function () {
+    board3.move('b8-c6')
+})
+
+$('#3move5').on('click', function () {
+    board3.move('f1-c4')
+})
+
+$('#3startPosition').on('click', board3.start)
 });
