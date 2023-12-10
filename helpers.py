086c2f75ce48111373192309e521ca38259e6c4c
DanielZhangg <143767413+DanielZhangg@users.noreply.github.com>
2023-12-10 13:24:53 -0500

    d

--- helpers.py
+++ helpers.py
@@ -30,6 +30,7 @@ def generate_board():
 
 def apology(message, code=400):
     """Render message as an apology to user."""
+    return render_template("apology.html", code=code, message=message)
 
     def escape(s):
         """
