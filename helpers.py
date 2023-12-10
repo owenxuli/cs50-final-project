b3885b1db740c4461c75e298af0b0a8a410cc0d0
DanielZhangg <143767413+DanielZhangg@users.noreply.github.com>
2023-12-10 13:26:17 -0500

    d

--- helpers.py
+++ helpers.py
@@ -32,27 +32,6 @@ def apology(message, code=400):
     """Render message as an apology to user."""
     return render_template("apology.html", code=code, message=message)
 
-    def escape(s):
-        """
-        Escape special characters.
-
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [
-            ("-", "--"),
-            (" ", "-"),
-            ("_", "__"),
-            ("?", "~q"),
-            ("%", "~p"),
-            ("#", "~h"),
-            ("/", "~s"),
-            ('"', "''"),
-        ]:
-            s = s.replace(old, new)
-        return s
-
-    return render_template("apology.html", top=code, bottom=escape(message)), code
-
 
 def login_required(f):
     """
